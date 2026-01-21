8b038f0e64b95e6ebd77090109895eb9fb29b680
ParvathiVeerakumar <parvathibalu1994@gmail.com>
2026-01-21 12:43:00 -0600

    pipeline folder added

---
+++ pipeline/pipeline.py
@@ -0,0 +1,11 @@
+import sys
+import pandas as pd
+
+print(f'system arguments: {sys.argv}')
+
+month =sys.argv[1]
+df = pd.DataFrame({'Day':[1,2], 'Num_Passengers':[23,34]})
+df['Month'] = month
+print(df)
+df.to_parquet(f"ouput_{month}.parquet")
+print(f'hello from pipeline. month={month}')
