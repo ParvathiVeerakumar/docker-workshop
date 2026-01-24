2176f493e0def2ff5b460b1ca8c653099f2847e5
ParvathiVeerakumar <parvathibalu1994@gmail.com>
2026-01-23 18:35:49 -0600

    created ingest data py and run the pipeline after docker compose

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -72,20 +73,32 @@ def ingest_data(
 
     print(f'done ingesting to {target_table}')
 
-def main():
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = '5432'
-    pg_db = 'ny_taxi'
-    year = 2021
-    month = 1
-    chunksize = 100000
-    target_table = 'yellow_taxi_data'
 
+@click.command()
+@click.option("--pg-user", default="root", show_default=True, help="Postgres user")
+@click.option("--pg-pass", default="root", show_default=True, help="Postgres password")
+@click.option("--pg-host", default="localhost", show_default=True, help="Postgres host")
+@click.option("--pg-port", default="5432", show_default=True, help="Postgres port")
+@click.option("--pg-db", default="ny_taxi", show_default=True, help="Postgres database name")
+@click.option("--year", default=2021, show_default=True, type=int, help="Data year")
+@click.option("--month", default=1, show_default=True, type=int, help="Data month (1-12)")
+@click.option("--chunksize", default=100000, show_default=True, type=int, help="CSV read chunksize")
+@click.option("--target-table", default="yellow_taxi_data", show_default=True, help="Target table name")
+
+def main(
+    pg_user: str,
+    pg_pass: str,
+    pg_host: str,
+    pg_port: str,
+    pg_db: str,
+    year: int,
+    month: int,
+    chunksize: int,
+    target_table: str,
+) -> None:
+    """Ingest NYC yellow taxi data into a Postgres table."""
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
-    url_prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
-
+    url_prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow"
     url = f'{url_prefix}/yellow_tripdata_{year:04d}-{month:02d}.csv.gz'
 
     ingest_data(
@@ -94,6 +107,7 @@ def main():
         target_table=target_table,
         chunksize=chunksize
     )
+
 
 if __name__ == '__main__':
     main()
